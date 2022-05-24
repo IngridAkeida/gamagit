3e6cda198d26a459edbae3a73a87546d8e3ba708
IngridAkeida <ingridsanchesp@gmail.com>
2022-05-24 16:00:53 +0200

    update commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 
 
 function App(props) {
+
+  //const [usuario, setUsuario ]= useState("Guilherme")
   return (
     <>
-      <h1>{props.title}</h1>
+      {/*<p>{usuario}</p>
+      <h1>{props.title}</h1>*/}
       <input name='usuario' id='usuario' className='usuarioInput' placeholder='Usuário'/>
+      
     </>
     
   );
