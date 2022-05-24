870885367a0ae9150558c4589efca11eabaa3ecb
IngridAkeida <ingridsanchesp@gmail.com>
2022-05-24 18:03:52 +0200

    import home

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
-import { useState } from "react";
-import axios from "axios";
+import Home from './Home';
 
 
-function App(props) {
+function App() {
 
-  const [usuario, setUsuario ]= useState("")
-
-  function handleSearch(){
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response=> console.log(response));
-  }
-  
   return (
-    <>
-    
-      {/*<p>{usuario}</p>
-      <h1>{props.title}</h1>*/}
-      
-      <input 
-        className='usuarioInput' 
-        placeholder='Usuário'
-        value={usuario}
-        onChange={e => setUsuario(e.target.value)}
-      />
-
-      <button type="button" onClick={handleSearch}>Pesquisar</button>
-   
+    <>     
+      <Home/>
     </>
     
   );
