5de21e61e2929b139b7f3909214642ae615cbc88
IngridAkeida <ingridsanchesp@gmail.com>
2022-05-25 16:18:54 +0200

    restructuring index

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  
     <App title="Hello Gama Academy"/>
-  </React.StrictMode>
+  
 );
 
 
